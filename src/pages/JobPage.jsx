37316d4d60b8751e7b87a2075b3fdffeb9af43f9
Jose Abrego <joseabrego13@gmail.com>
2025-05-09 21:53:45 -0500

    Fetched api data using data loader.

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,30 +1,41 @@
 import React from "react";
-import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+// import { useState, useEffect } from "react";
+import { useParams, useLoaderData } from "react-router-dom";
 import Spinner from "../components/Spinner";
 
 const JobPage = () => {
     const { id } = useParams();
-    const [job, setJob] = useState(null);
-    const [loading, setLoading] = useState(true);
+    const job = useLoaderData();
 
-    useEffect(() => {
-        const fetchJob = async () => {
-            try {
-                const res = await fetch(`/api/jobs/${id}`);
-                const data = await res.json();
-                setJob(data);
-            } catch (error) {
-                console.log("Error fetching data", data);
-            } finally {
-                setLoading(false);
-            }
-        };
+    // fetching using useEffect()
 
-        fetchJob();
-    }, []);
+    // const [job, setJob] = useState(null);
+    // const [loading, setLoading] = useState(true);
 
-    return loading ? <Spinner /> : <h1>{job.title}</h1>;
+    // useEffect(() => {
+    //     const fetchJob = async () => {
+    //         try {
+    //             const res = await fetch(`/api/jobs/${id}`);
+    //             const data = await res.json();
+    //             setJob(data);
+    //         } catch (error) {
+    //             console.log("Error fetching data", data);
+    //         } finally {
+    //             setLoading(false);
+    //         }
+    //     };
+
+    //     fetchJob();
+    // }, []);
+
+    return <h1>{job.title}</h1>;
 };
 
-export default JobPage;
+// Fetching using a Data Loader
+const jobLoader = async ({ params }) => {
+    const res = await fetch(`/api/jobs/${params.id}`);
+    const data = await res.json();
+    return data;
+};
+
+export { JobPage as default, jobLoader };
