2faa3f5e4b838e6be09f5291ef9020592866e42d
Jose Abrego <joseabrego13@gmail.com>
2025-05-19 21:43:17 -0500

    Implemented toast messages for add and delete.

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 // import { useState, useEffect } from "react";
 import { useParams, useLoaderData, Link, useNavigate } from "react-router-dom";
 import { FaArrowLeft, FaMapMarker } from "react-icons/fa";
+import { toast } from "react-toastify";
 
 const JobPage = ({ deleteJob }) => {
     const navigate = useNavigate();
@@ -16,6 +17,9 @@ const JobPage = ({ deleteJob }) => {
         if (!confirm) return;
 
         deleteJob(jobId);
+
+        toast.success("Job Deleted Successfully");
+
         navigate("/jobs");
     };
 
