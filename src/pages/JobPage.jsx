e35205aa9c7f3fbddecfa4e15bbeeab2d423cbc9
Jose Abrego <joseabrego13@gmail.com>
2025-05-20 16:58:04 -0500

    Fixed edit job page link.

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -127,7 +127,7 @@ const JobPage = ({ deleteJob }) => {
                                     Manage Job
                                 </h3>
                                 <Link
-                                    to={`/jobs/edit/${job.id}`}
+                                    to={`/edit-job/${job.id}`}
                                     className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
                                 >
                                     Edit Job
