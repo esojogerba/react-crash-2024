183a7a618e8241d579b8d7846001bbd885281c8c
Jose Abrego <joseabrego13@gmail.com>
2025-05-21 06:59:11 -0500

    Implemented updateJob function.

--- src/App.jsx
+++ src/App.jsx
@@ -34,6 +34,17 @@ const App = () => {
         return;
     };
 
+    const updateJob = async (job) => {
+        const res = await fetch(`/api/jobs/${job.id}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(job),
+        });
+        return;
+    };
+
     const router = createBrowserRouter(
         createRoutesFromElements(
             <Route path="/" element={<MainLayout />}>
@@ -50,7 +61,7 @@ const App = () => {
                 />
                 <Route
                     path="/edit-job/:id"
-                    element={<EditJobPage />}
+                    element={<EditJobPage updateJobSubmit={updateJob} />}
                     loader={jobLoader}
                 />
                 <Route path="*" element={<NotFoundPage />} />
